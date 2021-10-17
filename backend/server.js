ab993e7cc996f9de6a0cba8970626020c3e3409a
Snehal Narute <snehaln5551997@gmail.com>
2021-10-18 01:33:07 +0530

    modified package json start script and app name

--- backend/server.js
+++ backend/server.js
@@ -22,20 +22,7 @@ app.use('/api/users', userRoutes)
 app.get('/api/courses', (req, res) => {
 	return res.json(courses)
 })
-// --------------------------deployment------------------------------
-const __dirname = path.resolve()
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-	)
-} else {
-	app.get('/', (req, res) => {
-		res.send('API is running..')
-	})
-}
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
@@ -44,9 +31,4 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
-	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
-	)
-)
+app.listen(PORT, console.log(`Server running on port ${PORT}..`.yellow))
